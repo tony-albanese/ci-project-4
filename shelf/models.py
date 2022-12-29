1069a3f411503e99f75cb93f6c8de74c485580fd
tony-albanese <38885564+tony-albanese@users.noreply.github.com>
2022-12-29 10:44:29 +0100

    Models (#5)
    * set python beautifier
    
    * add Book model
    
    * add Comment model
    
    * refactor field name
    
    * add Shelf model
    add magic methodds to Book model
    
    * complete TODOs
    
    * Add TODO
    
    * Refactor model for one to many relationship
    
    * delete whitespace
    
    * add str method to Comment model
    
    * set related_name to +
    
    * Initial commit
    migration after creating models
    
    * register Book model for admin panel
    
    * add test table for models to readme

--- shelf/models.py
+++ shelf/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Shelf(models.Model):
+    title = models.CharField(max_length=100, default="My Shelf")
+
+
+class Book(models.Model):
+    GENRES = [
+        ('fan', 'Fantasy'),
+        ('adv', 'Adventure'),
+        ('rom', 'Romance'),
+        ('dys', 'Dystopian'),
+        ('mys', 'Mystery'),
+        ('hor', 'Horror'),
+        ('thrill', 'Thriller'),
+        ('hisfic', 'Historical Fiction'),
+        ('scifi', 'Science Fiction'),
+        ('bio', 'Biography'),
+        ('cook', 'Cookbook'),
+        ('mot', 'Motivational'),
+        ('health', 'Health'),
+        ('his', 'history'),
+        ('travel', 'Travel'),
+        ('diy', 'How-to'),
+    ]
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+    genre = models.CharField(max_length=6, choices=GENRES)
+    likes = models.ManyToManyField(User, blank=True)
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def number_of_like(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    body = models.TextField()
+    written_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['written_on']
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
+
