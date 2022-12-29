211d3851437b61c4679d8536ce2a47290f948f0f
tony-albanese <tony.albanese2000@gmail.com>
2022-12-29 10:12:17 +0000

    Add settings for allauth

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from shelf.views import hello_bookshelf, hello_bookshelf_template
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_bookshelf, name='hello_bookshelf'),
     path('hello-template/', hello_bookshelf_template, name='hello_bookshelf_template'),
+    path('accounts/', include('allauth.urls')),
+
 ]
