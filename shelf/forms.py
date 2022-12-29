5178921b854e755c2881871737f5c5e3199a5e6c
tony-albanese <tony.albanese2000@gmail.com>
2022-12-29 17:36:04 +0000

    initial commit. For auto generate form from model

---
+++ shelf/forms.py
@@ -0,0 +1,9 @@
+from . models import Book
+from django import forms
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('title', 'author', 'description', 'genre')
+
